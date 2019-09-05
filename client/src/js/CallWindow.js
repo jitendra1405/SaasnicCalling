f939a25bb3f3cb5e790e9baf003bc71c997e4e93
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-05 13:42:31 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -178,11 +178,14 @@ btnstartrecording() {
             className="btn-action hangup fa fa-phone"
             onClick={() => endCall(true)}
           />
-    <button
+<svg height="100" width="100">
+  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+    
+<button
             type="button"
-            className="{getClass(playpause)}"
+            
             onClick={() => this.btnstartrecording()}
-          />
+          /></svg>
         <button
             type="button"
             className="btn-action hangup fa fa-phone"
