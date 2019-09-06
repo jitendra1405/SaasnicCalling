6eb074f87d3ca02636508eaca01aa7185c26f3d2
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-06 10:56:10 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -28,8 +28,7 @@ module.exports = () => {
   const MIN = 1000;
   const MAX = 9999;
   const num = Math.floor(Math.random() * ((MAX + 1) - MIN)) + MIN;
-  const abc = 'saasnic';
-  const efd = 'tech';
-  return `${abc}`;
-  return `${efd}`;
+  
+  return `${adj}-${noun}-${num}`;
+  
 };
