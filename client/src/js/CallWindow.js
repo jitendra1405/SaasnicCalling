1fd6d58b7874fab8ebce070b852d38e2530c4bab
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-06 11:46:47 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -93,10 +93,10 @@ btnstartrecording() {
  if(typeof RecordRTC_Extension === 'undefined') {
    <button id="myBtn">Open Modal</button>
 
-<!-- The Modal -->
+
 <div id="myModal" class="modal">
 
-  <!-- Modal content -->
+  
   <div class="modal-content">
     <span class="close">&times;</span>
     <p>Some text in the Modal..</p>
