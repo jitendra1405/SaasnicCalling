1bfd8d9ee058b3aa8a66c13c821eff7e4ae61065
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-09 11:03:12 +0530

    Update CallModal.js

--- client/src/js/CallModal.js
+++ client/src/js/CallModal.js
@@ -31,17 +31,7 @@ function CallModal({ status, callFrom, startCall,startCall12, rejectCall }) {
          
   };
   
-  fuction data(){
-    var connectionString = "postgres://*leeglxtkajgvtl*:*76f29beea03eb3bd5b69672f0d292a01ae95d251957282df96e882864c969e50*@*ec2-23-21-156-171.compute-1.amazonaws.com*:*5432*/*daff54nelb3ps6*";
-
-pg.connect(connectionString, function(err, client, done) {
-   client.query('SELECT email,lastname FROM webrtc.contact', function(err, result) {
-      done();
-      if(err) return console.error(err);
-      console.log(result.rows);
-   });
-});
-  }
+  
   
   const rejectCall11 = (video) => {
     
@@ -78,7 +68,17 @@ pg.connect(connectionString, function(err, client, done) {
   );
 
 }
+function data(){
+    var connectionString = "postgres://*leeglxtkajgvtl*:*76f29beea03eb3bd5b69672f0d292a01ae95d251957282df96e882864c969e50*@*ec2-23-21-156-171.compute-1.amazonaws.com*:*5432*/*daff54nelb3ps6*";
 
+pg.connect(connectionString, function(err, client, done) {
+   client.query('SELECT email,lastname FROM webrtc.contact', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   });
+});
+  }
 CallModal.propTypes = {
  
 
