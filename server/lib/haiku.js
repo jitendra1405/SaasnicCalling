dbfaa0eba100a6af9975207ade0b7784e491915c
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-09 16:48:31 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -23,6 +23,7 @@ const nouns = [
 ];
 
 module.exports = () => {
+  console.log('i am in haiku');
   const adj = adjs[Math.floor(Math.random() * adjs.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
   const MIN = 1000;
