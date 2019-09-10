090a9082b884380159b6c815f9b987bb9f272394
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-10 13:46:00 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -2,29 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'proptypes';
 
 let friendID;
-var connString = 'postgres://leeglxtkajgvtl:76f29beea03eb3bd5b69672f0d292a01ae95d251957282df96e882864c969e50@ec2-23-21-156-171.compute-1.amazonaws.com:5432/daff54nelb3ps6';
-
-var pg = require('pg');
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-
-	pg.connect(connString, function(err, client, done) {
-		if(err) response.send("Could not connect to DB: " + err);
-		client.query('SELECT * FROM MyTable', function(err, result) {
-			done();
-			if(err) return response.send(err);
-			response.send(result.rows);
-		});
-	});
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});
 
 class MainWindow extends Component {
   /**
