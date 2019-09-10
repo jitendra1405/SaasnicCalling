b91890f3b036671083f3a05e07905bcc4280d42e
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-10 17:30:40 +0530

    Create test.php

---
+++ client/src/html/test.php
@@ -0,0 +1,19 @@
+<?php
+$dbconn = pg_connect("host=ec2-54-225-72-238.compute-1.amazonaws.com port=5432 dbname=d1mbimqnj4bo69 user=oyymgxywhiwmff password=5fcdb5e030395d64b21992644afe083d537353d7a0653755c0a166b088a826a3");
+			
+                     $sql = "select firstname from contact.contact";
+			    
+                            $resultset = pg_query($dbconn, $sql);
+                            while($row = pg_fetch_array($resultset)) {
+                                
+				  echo '<tr>
+                                        <td>'.$row[0].'</td>
+                                        
+                                    </tr>'; 
+                            }
+				echo 'hello';
+				
+		               
+                            pg_close($dbconn); 
+	   }
+     ?>
