9945a62d8590d103a844cf9d1b619fa3ae3a286f
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-11 13:21:50 +0530

    Update CallModal.js

--- client/src/js/CallModal.js
+++ client/src/js/CallModal.js
@@ -7,6 +7,33 @@ import CallWindow from './CallWindow';
 
 function CallModal({ status, callFrom, startCall,startCall12, rejectCall }) {
   console.log('theriiii',status,rejectCall);
+  var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    database : 'dbname',
+    user     : 'username',
+    password : 'password',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM employee', function (error, results, fields) {
+    if (error)
+        throw error;
+
+    results.forEach(result => {
+        console.log(result);
+    });
+});
+
+connection.end();
  
   var audio = new Audio('http://hipehome.com/nokia-ringtone-2019-256k-46684.mp3');
   if(status == " "){
