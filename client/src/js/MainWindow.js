1a246ec219a360b46e030d87a2a9307feb58a9e0
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-11 18:34:03 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -1,57 +1,9 @@
-"use strict";
+
 import React, { Component } from 'react';
 import PropTypes from 'proptypes';
 
 let friendID;
 
-const fs = require('fs');
-const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
-
-fs.readFile(f, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
-
-  fs.writeFile(f, result, 'utf8', function (err) {
-    if (err) return console.log(err);
-  });
-});
-
-const url = require('url');
-const pg = require('pg');
-
-const params = url.parse("postgres://leeglxtkajgvtl:76f29beea03eb3bd5b69672f0d292a01ae95d251957282df96e882864c969e50@ec2-23-21-156-171.compute-1.amazonaws.com:5432/daff54nelb3ps6");
-const auth = params.auth.split(':');
-
-const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1]
-};
-
-const pool = new pg.Pool(config);
-
-const DB = {
-    query: function(query, callback) {
-        pool.connect((err, client, done) => {
-            if(err) return callback(err)
-            client.query(query, (err, results) => {
-                done()
-                if(err) { console.error("ERROR: ", err) }
-                if(err) { return callback(err) }
-                callback(null, results.rows)
-            })
-        });
-    }
-}
-
-let query = "SELECT * FROM webrtc.contact;"
-query(query, (err, results) => {
-    console.log('ppppppppppppppppppppppppppppppppppppppp',results);
-})
 class MainWindow extends Component {
   /**
    * Start the call with or without video
