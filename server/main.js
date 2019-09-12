fc723ae9a57e89e65c9bc329bbb63bb7f5033904
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-12 12:03:47 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,21 +1,4 @@
 const config = require('../config.json');
 const server = require('./lib/server');
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT * FROM webrtc.contact;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
- 
 server.run(config);
