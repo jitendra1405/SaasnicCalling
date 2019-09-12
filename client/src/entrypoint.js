a107fdd7507846d8195d03f430007b418cf36252
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-12 11:48:28 +0530

    Update entrypoint.js

--- client/src/entrypoint.js
+++ client/src/entrypoint.js
@@ -1,2 +1,26 @@
 import './js/app';
 import './css/app.scss';
+
+connectionString = {
+connectionString: process.env.DATABASE_URL,
+ssl: true
+};
+const { Pool } = require('pg'); 
+const secrets = require('../middleware/ENV').default;
+const env = process.env.NODE_ENV || 'development';
+let connectionString = {
+    user: secrets.user,
+    database: secrets.testDb,
+    host: secrets.host
+};
+// checking to know the environment and suitable connection string to use
+if (env === 'development') {
+    connectionString.database = secrets.database;
+} else {
+    connectionString = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+    };
+};
+const pool = new Pool(connectionString);
+pool.on('connect', () => console.log('connected to db'));
